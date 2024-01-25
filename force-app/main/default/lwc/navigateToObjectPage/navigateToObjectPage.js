03c00158fae1f77c2a0a44c4cffa3dd3a41204d3
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-01-25 12:27:17 +0100

    Add navigate to new record with default values

--- force-app/main/default/lwc/navigateToObjectPage/navigateToObjectPage.js
+++ force-app/main/default/lwc/navigateToObjectPage/navigateToObjectPage.js
@@ -1,5 +1,6 @@
 import { LightningElement } from "lwc";
 import { NavigationMixin } from "lightning/navigation";
+import { encodeDefaultFieldValues } from "lightning/pageReferenceUtils";
 
 export default class NavigateToObjectPage extends NavigationMixin(
   LightningElement
@@ -10,6 +11,28 @@ export default class NavigateToObjectPage extends NavigationMixin(
       attributes: {
         objectApiName: "Contact",
         actionName: "new"
+      }
+    });
+  }
+
+  navigateToNewRecordWithDefault() {
+    const defaultValue = encodeDefaultFieldValues({
+      // Visit 'Setup > Object Manager > Contact > Fields & Relationships' in app to see other fields API names.
+      FirstName: "Habeeb",
+      LastName: "Kareem",
+      LeadSource: "Other",
+      Title: "Software Engineer",
+      Salutation: "Mr",
+      MobilePhone: +2347082163310
+    });
+    this[NavigationMixin.Navigate]({
+      type: "standard__objectPage",
+      attributes: {
+        objectApiName: "Contact",
+        actionName: "new"
+      },
+      state: {
+        defaultFieldValues: defaultValue
       }
     });
   }

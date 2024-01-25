2cb9ab939a9716d3faf6b69a22556f3574903dcd
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-01-25 21:49:22 +0100

    @wire is reactive

--- force-app/main/default/lwc/wireDemoUserDetail/wireDemoUserDetail.js
+++ force-app/main/default/lwc/wireDemoUserDetail/wireDemoUserDetail.js
@@ -6,18 +6,20 @@ import EMAIL_FIELD from "@salesforce/schema/User.Email";
 const fields = [NAME_FIELD, EMAIL_FIELD];
 export default class WireDemoUserDetail extends LightningElement {
   userId = Id;
+  //0057E00000GHXhbQAH"
 
   userDetail;
-  @wire(getRecord, { recordId: "0037E00001Bapt4QAB", fields })
+  @wire(getRecord, { recordId: "$userId", fields }) // shows that "$userId" of the userId for the @wire method is reactive.
   userDetailHandler({ data, error }) {
     if (data) {
       this.userDetail = data.fields;
+      //   console.log(data);
     }
     if (error) {
       console.error(error);
     }
   }
 
-  @wire(getRecord, { recordId: "0037E00001Bapt4QAB", fields })
+  @wire(getRecord, { recordId: "$userId", fields })
   userDetailProperty;
 }

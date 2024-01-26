c28485058a4402f619838808cd5e675d1b22469f
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-01-26 20:36:18 +0100

    import Apex method to LWC

---
+++ force-app/main/default/lwc/apexWireDemo/apexWireDemo.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+import { LightningElement } from "lwc";
+import getAccountList from "@salesforce/apex/AccountController.getAccountList";
+
+export default class ApexWireDemo extends LightningElement {}

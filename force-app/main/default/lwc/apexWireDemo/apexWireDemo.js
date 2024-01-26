3306e89598c111acbd555345ac295f9e51a4d561
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-01-26 21:13:15 +0100

    Wire Apex method

--- force-app/main/default/lwc/apexWireDemo/apexWireDemo.js
+++ force-app/main/default/lwc/apexWireDemo/apexWireDemo.js
@@ -1,5 +1,27 @@
-/* eslint-disable no-unused-vars */
-import { LightningElement } from "lwc";
+import { LightningElement, wire } from "lwc";
 import getAccountList from "@salesforce/apex/AccountController.getAccountList";
+export default class ApexWireDemo extends LightningElement {
+  accountList;
 
-export default class ApexWireDemo extends LightningElement {}
+  @wire(getAccountList)
+  accounts;
+
+  @wire(getAccountList)
+  accountsHandler({ data, error }) {
+    if (data) {
+      console.log(data);
+      this.accountList = data.map((item) => {
+        let newType =
+          item.Type === "Customer - Channel"
+            ? "Channel"
+            : item.Type === "Customer - Direct"
+              ? "Direct"
+              : "-------";
+        return { ...item, newType };
+      });
+    }
+    if (error) {
+      console.error(error);
+    }
+  }
+}

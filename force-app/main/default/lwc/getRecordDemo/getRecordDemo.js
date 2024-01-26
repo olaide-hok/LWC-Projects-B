7a32e63a53a1f6e9324a7c2e51858243485798c1
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-01-26 07:35:00 +0100

    Use getFieldValue and getFieldDeisplayValue APIs

--- force-app/main/default/lwc/getRecordDemo/getRecordDemo.js
+++ force-app/main/default/lwc/getRecordDemo/getRecordDemo.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-unused-vars */
 import { LightningElement, wire, api } from "lwc";
-import { getRecord } from "lightning/uiRecordApi";
+import {
+  getRecord,
+  getFieldValue,
+  getFieldDisplayValue
+} from "lightning/uiRecordApi";
 import NAME_FIELD from "@salesforce/schema/Account.Name";
 import OWNER_NAME_FIELD from "@salesforce/schema/Account.Owner.Name";
 import ANNUAL_REVENUE_FIELD from "@salesforce/schema/Account.AnnualRevenue";
@@ -9,25 +13,37 @@ export default class GetRecordDemo extends LightningElement {
   owner;
   AnnualRevenue;
   @api recordId;
-  // @wire(getRecord, {recordId:'$recordId',
-  //  fields:[NAME_FIELD, OWNER_NAME_FIELD, ANNUAL_REVENUE_FIELD]})
   @wire(getRecord, {
     recordId: "$recordId",
-    layoutTypes: ["Full"],
-    modes: ["View"]
+    fields: [NAME_FIELD, OWNER_NAME_FIELD, ANNUAL_REVENUE_FIELD]
   })
+  //   @wire(getRecord, {
+  //     recordId: "$recordId",
+  //     layoutTypes: ["Full"],
+  //     modes: ["View"]
+  //   })
   accountHandler({ data }) {
     if (data) {
       console.log(data);
-      this.name = data.fields.Name.displayValue
-        ? data.fields.Name.displayValue
-        : data.fields.Name.value;
-      this.AnnualRevenue = data.fields.AnnualRevenue.displayValue
-        ? data.fields.AnnualRevenue.displayValue
-        : data.fields.AnnualRevenue.value;
-      this.owner = data.fields.Owner.displayValue
-        ? data.fields.Owner.displayValue
-        : data.fields.Owner.value;
+      //   this.name = data.fields.Name.displayValue
+      //     ? data.fields.Name.displayValue
+      //     : data.fields.Name.value;
+      //   this.AnnualRevenue = data.fields.AnnualRevenue.displayValue
+      //     ? data.fields.AnnualRevenue.displayValue
+      //     : data.fields.AnnualRevenue.value;
+      //   this.owner = data.fields.Owner.displayValue
+      //     ? data.fields.Owner.displayValue
+      //     : data.fields.Owner.value;
+
+      //   this.name = getFieldValue(data, NAME_FIELD);
+      //   this.AnnualRevenue = getFieldValue(data, ANNUAL_REVENUE_FIELD);
+      //   this.owner = getFieldValue(data, OWNER_NAME_FIELD);
+
+      this.name = getFieldDisplayValue(data, NAME_FIELD)
+        ? getFieldDisplayValue(data, NAME_FIELD)
+        : getFieldValue(data, NAME_FIELD);
+      this.AnnualRevenue = getFieldDisplayValue(data, ANNUAL_REVENUE_FIELD);
+      this.owner = getFieldValue(data, OWNER_NAME_FIELD);
     }
   }
 }
